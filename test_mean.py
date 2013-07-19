3eeebcefc2e03ef6f6c5315c958186783a900cce
IS Kim <iskim@post.harvard.edu>
2013-07-19 15:52:39 -0700

    added to test_mean.py emptylist and numlist tests

--- test_mean.py
+++ test_mean.py
@@ -24,3 +24,21 @@ def test_floating_mean1():
     obs = mean([1, 2])
     exp = 1.5
     assert_equal(obs, exp)
+
+def test_isnotempty():
+	assert_raises(ZeroDivisionError, mean, [])
+	
+def test_isnumlist():
+	assert_raises(TypeError, mean, ['1', 'banana', 3])
+	assert_raises(TypeError, mean, 'banana')
+	assert_raises(TypeError, mean, 1)
+
+def main():
+    test_mean1()
+    test_floating_mean1()
+    test_isnotempty()
+    test_isnumlist()
+    print "we ran the main"
+
+if __name__ == "__main__":
+    main()
